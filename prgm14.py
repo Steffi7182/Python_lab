093da4873814c4b91c717c09e2c13f8e3c7736ea
Steffi Antony <144442545+Steffi7182@users.noreply.github.com>
2023-10-10 11:17:54 +0530

    Add files via upload

---
+++ prgm14.py
@@ -0,0 +1,4 @@
+color_list1 = set(["black","white","red","blue"])
+color_list2 = set(["black","brown","red","pink"])
+print("color_list1 not contained in color_list2 are:")
+print(color_list1.difference(color_list2))
